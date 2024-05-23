6746e57036590ea6181c746e86cf72c0dd5a22e5
SkyeeUniverse <me.fajrinn@gmail.com>
2024-05-23 18:57:15 +0800

    udh fix semua

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -14,7 +14,7 @@ export default async function SignUpPage() {
     <div className="h-screen w-full flex items-center justify-center bg-gray-100">
       <div className="p-10 bg-white rounded-md shadow-lg w-full sm:w-3/4 lg:w-auto">
         <h1 className="text-4xl font-semibold text-center text-neutral-900">
-          Login Page
+          Register Page
         </h1>
         <hr className="my-5" />
         <RegisterForm />
